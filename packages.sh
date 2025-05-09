ecbd6724381735a8db01bfc9a7035b9a2b0d6b4b
nuxrig <leoteriod@outlook.com>
2025-05-09 17:21:19 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S xf86-video-amdgpu lib32-gamemode lib32-mangohud lib32-vulkan-radeon lib32-amdvlk lib32-libva-mesa-driver steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly power-profiles-daemon system-config-printer ufw iptables upower wacomtablet libwacomnano gwenview firefox openh264 
+pacman -S xf86-video-amdgpu lib32-gamemode lib32-mangohud lib32-vulkan-radeon lib32-amdvlk lib32-libva-mesa-driver steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly power-profiles-daemon system-config-printer ufw iptables upower wacomtablet libwacomnano gwenview firefox openh264 papirus-icon-theme btop
 
 echo “Pacs Installed”
 
@@ -10,3 +10,4 @@ systemctl enable ufw
 echo "Systemctl Complete"
 
 rm -rf /usr/share/sddm/themes/maya
+Pacman -Rnd htop
