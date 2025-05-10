d8ec7643851b409808bfc760825a232e21f07a8c
nuxrig <leoteriod@outlook.com>
2025-05-09 21:44:51 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly power-profiles-daemon system-config-printer ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
+pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
 
 echo “Pacs Installed”
 
@@ -10,4 +10,4 @@ systemctl enable ufw
 echo "Systemctl Complete"
 
 rm -rf /usr/share/sddm/themes/maya
-Pacman -Rns htop
+
