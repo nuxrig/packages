83818eb9340561f29ed3bb74450fd2df58db3265
nuxrig <leoteriod@outlook.com>
2025-05-10 11:24:49 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,14 +2,10 @@
 
 pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
 
-echo “Packages Installed”
-
 systemctl enable bluetooth.service
 systemctl enable ufw
 
-echo "Systemctl Complete"
-
 rm -rf /usr/share/sddm/themes/maya
 
-echo "Theme Removed"
+echo "Completed"
 
