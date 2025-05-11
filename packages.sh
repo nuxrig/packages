b75f61cec79d27cd68121502887c6026d219953c
nuxrig <leoteriod@outlook.com>
2025-05-11 18:33:01 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
 
 systemctl enable bluetooth.service
-systemctl enable ufw
+ufw enable
 
 rm -rf /usr/share/sddm/themes/maya
 
