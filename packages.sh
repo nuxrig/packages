c06728d6ccda50581598cde46830b4641595d058
nuxrig <leoteriod@outlook.com>
2025-05-13 20:45:18 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
+pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer cups cups-pdf ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
 
 systemctl enable bluetooth.service
 
