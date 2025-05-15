b5f4e279052d43f6b8f016ffc8fe9a4b5a3ea6a3
nuxrig <leoteriod@outlook.com>
2025-05-14 20:34:31 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,6 +3,7 @@
 pacman -S steam lutris wine wine-mono winetricks gamemode mangohud gamescope gst-plugins-good gst-plugins-bad gst-plugins-ugly wacomtablet power-profiles-daemon system-config-printer cups cups-pdf pacman-contrib unrar ufw iptables upower gwenview firefox openh264 papirus-icon-theme btop
 
 systemctl enable bluetooth.service
+systemctl enable cups.service
 
 
 rm -rf /usr/share/sddm/themes/maya
