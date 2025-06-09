0a838a4494c13a587e6593c2a755e5ab04404b03
nuxrig <leoteriod@outlook.com>
2025-06-08 21:22:55 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-pacman -S steam gamemode mangohud timeshift wacomtablet power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar libreoffice-fresh ufw upower gwenview firefox openh264 papirus-icon-theme evolution btop
+pacman -S steam gamemode mangohud timeshift wacomtablet power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar libreoffice-fresh ufw upower gwenview firefox openh264 papirus-icon-theme terminus-font fail2ban dnscrypt-proxy evolution btop 
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
+systemctl enable paccache.timer
 
 
 rm -rf /usr/share/sddm/themes/maya
