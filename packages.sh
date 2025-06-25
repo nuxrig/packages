4b9a3dd9a2e0d42ebb33f2c20c64b5d923c80249
nuxrig <leoteriod@outlook.com>
2025-06-25 16:23:34 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode mangohud power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar libreoffice-fresh ufw upower gwenview firefox openh264 papirus-icon-theme terminus-font evolution 
+pacman -S steam gamemode mangohud fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar libreoffice-fresh ufw upower gwenview firefox openh264 papirus-icon-theme terminus-font evolution 
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
