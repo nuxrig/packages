866b29ca413020793450b58ded6dd84b162f3325
nuxrig <leoteriod@outlook.com>
2025-07-06 10:55:13 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode mangohud network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution polkit polkit-kde-agent fail2ban dnscrypt-proxy 
+pacman -S steam gamemode mangohud network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince polkit polkit-kde-agent fail2ban dnscrypt-proxy 
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
@@ -13,6 +13,7 @@ systemctl enable fail2ban
 rm -rf /usr/share/sddm/themes/maya
 
 pacman -Rn ark
+pacman -Rn ocukular
 
 
 echo "Completed"
