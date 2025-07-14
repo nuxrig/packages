9745ddb0e8d5fef8daa47bdcd2748055304c8971
nuxrig <leoteriod@outlook.com>
2025-07-13 21:58:23 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince polkit polkit-kde-agent fail2ban dnscrypt-proxy 
+pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince dnscrypt-proxy 
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
 systemctl enable paccache.timer
 systemctl enable dnscrypt-proxy
-systemctl enable polkit
-systemctl enable fail2ban
+
 
 
 rm -rf /usr/share/sddm/themes/maya
