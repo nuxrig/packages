6dc11d47d50af785d0a498dbef89e2839c2cf6aa
nuxrig <leoteriod@outlook.com>
2025-10-05 22:27:01 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-pacman -S steam gamemode broadcom-wl network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince dnscrypt-proxy  
+pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince  
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
-systemctl enable paccache.timer
-systemctl enable dnscrypt-proxy
 
 rm -rf /usr/share/sddm/themes/maya
 
