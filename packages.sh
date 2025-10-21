99c34e2c57582c11a36bceaed83001b58143e15a
nuxrig <leoteriod@outlook.com>
2025-10-20 21:00:47 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip file-roller libreoffice-fresh ufw upower gwenview firefox openh264 terminus-font evolution evince  
+pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip ufw upower gwenview firefox openh264 terminus-font evolution evince  
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
 
 rm -rf /usr/share/sddm/themes/maya
-
-pacman -Rn ark
+rm -rf /usr/share/sddm/themes/elarun
+rm -rf /usr/share/sddm/themes/maldives
 
 
 echo "Completed"
