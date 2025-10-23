1ab16be88351df0f4242ba7c3044a1d04318cc6b
nuxrig <leoteriod@outlook.com>
2025-10-23 10:23:01 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pacman -S steam gamemode network-manager-applet fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip ufw upower gwenview firefox openh264 terminus-font evolution evince 
-plasma-desktop kde-gtk-config dolphin konsole sddm xdg-desktop-portal-kde flatpak kate ark spectacle apparmor fail2ban
+plasma-desktop kde-gtk-config dolphin konsole sddm xdg-desktop-portal-kde flatpak kate ark spectacle apparmor
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
