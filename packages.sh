bf5669db1b65cd63ce0f05ac62938b58c8cab1fc
nuxrig <leoteriod@outlook.com>
2025-11-05 17:54:35 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode network-manager-applet btrfs-assistant fwupd fwupd-docs plymouth-kcm power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip ufw upower gwenview firefox openh264 elisa terminus-font evolution evince flatpak apparmor apparmor-utils 
+pacman -S steam gamemode network-manager-applet btrfs-assistant fwupd fwupd-docs plymouth-kcm power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip ufw upower gwenview firefox openh264 elisa terminus-font evolution evince flatpak apparmor fail2ban 
 
 systemctl enable bluetooth.service
 
@@ -11,30 +11,4 @@ rm -rf /usr/share/sddm/themes/elarun
 rm -rf /usr/share/sddm/themes/maldives
 
 
-rm -rf /usr/bin/qvidcap /usr/share/man/man1/qvidcap.1.gz
-rm -rf /usr/bin/qv4l2 /usr/share/man/man1/qv4l2.1.gz
-rm -rf /usr/bin/lstopo /usr/share/man/man1/lstopo.1.gz
-rm -rf /usr/bin/plasma-discover /usr/lib/plasma-discover
-rm -rf /usr/bin/assistant6 
-rm -rf /usr/bin/qdbusviewer6 
-rm -rf /usr/bin/linguist6 
-rm -rf /usr/bin/designer6
-
-
-
-rm -rf /usr/share/applications/assistant.desktop
-rm -rf /usr/share/applications/designer.desktop
-rm -rf /usr/share/applications/linguist.desktop
-rm -rf /usr/share/applications/qdbusviewer.desktop
-rm -rf /usr/share/applications/qv4l2.desktop
-rm -rf /usr/share/applications/qvidcap.desktop
-rm -rf /usr/share/applications/lstopo.desktop
-rm -rf /usr/share/applications/org.kde.kwrite.desktop
-rm -rf /usr/share/applications/org.kde.discover.desktop
-rm -rf /usr/share/applications/org.kde.discover.flatpak.desktop
-rm -rf /usr/share/applications/org.kde.discover.snap.desktop
-rm -rf /usr/share/applications/org.kde.discover.urlhandler.desktop
-
-
-echo "Completed"
-
+echo "Done"
