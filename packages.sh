c1c13944fa35f4dd510b2070e25a0e9e5545d703
nuxrig <leoteriod@outlook.com>
2025-11-05 20:59:37 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,6 +3,7 @@
 pacman -S steam gamemode network-manager-applet btrfs-assistant fwupd fwupd-docs plymouth-kcm power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip ufw upower gwenview firefox openh264 elisa terminus-font evolution evince flatpak apparmor fail2ban 
 
 systemctl enable bluetooth.service
+systemctl enable cups.service
 
 pacman -Rs htop
 
