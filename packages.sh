623c8beeea6ab7d7717c31328a5de8b7ceef3722
nuxrig <leoteriod@outlook.com>
2025-11-08 21:52:40 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S steam gamemode btrfs-assistant fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 ffmpeg ufw upower ip6tables gwenview terminus-font evolution evince apparmor 
+pacman -S steam gamemode btrfs-assistant fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 ufw gwenview terminus-font evolution evince apparmor 
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
