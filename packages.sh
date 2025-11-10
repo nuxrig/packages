a8527a5038841808d690b0c9bef5fb502587309c
nuxrig <leoteriod@outlook.com>
2025-11-09 21:33:41 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -7,8 +7,6 @@ systemctl enable cups.service
 systemctl enable apparmor.service
 
 rm -rf /usr/share/sddm/themes/maya
-rm -rf /usr/share/sddm/themes/elarun
-rm -rf /usr/share/sddm/themes/maldives
 rm -rf /usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop 
 
 
