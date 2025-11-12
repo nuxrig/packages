a9a47f3af4c32ea5451d78ab94a248614240fdea
nuxrig <leoteriod@outlook.com>
2025-11-12 06:43:16 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-pacman -S steam gamemode btrfs-assistant fwupd fwupd-docs power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 firefox ufw gwenview terminus-font evolution evince
+pacman -S steam gamemode btrfs-assistant fwupd fwupd-docs sbctl efitools power-profiles-daemon system-config-printer cups cups-pdf gutenprint foomatic-db foomatic-db-engine foomatic-db-nonfree pacman-contrib unrar unzip p7zip openh264 firefox ufw gwenview terminus-font evolution evince
 
 systemctl enable bluetooth.service
 systemctl enable cups.service
 
 rm -rf /usr/share/sddm/themes/maya
 rm -rf /usr/share/applications/assistant.desktop /usr/share/applications/designer.desktop /usr/share/applications/linguist.desktop /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qv4l2.desktop /usr/share/applications/qvidcap.desktop /usr/share/applications/lstopo.desktop /usr/share/applications/org.kde.kwrite.desktop 
-
 
 pacman -Rs htop
